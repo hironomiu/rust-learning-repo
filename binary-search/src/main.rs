67e7768966b8bde2fd84253152d8f7e57f91ab01
hironomiu <hironomiu@gmail.com>
2022-06-26 09:07:30 +0900

    commited

--- binary-search/src/main.rs
+++ binary-search/src/main.rs
@@ -1,7 +1,9 @@
 fn binary_search(nums: &Vec<i32>, target: i32) -> i32 {
     let (mut min, mut max) = (0, nums.len());
     while min <= max {
-        let mid = (min + max) / 2;
+        // MEMO: シフト演算
+        // let mid = (min + max) / 2;
+        let mid = min + max >> 1;
         if nums[mid] == target {
             return mid as i32;
         } else if nums[mid] < target {
@@ -13,8 +15,11 @@ fn binary_search(nums: &Vec<i32>, target: i32) -> i32 {
     return -1;
 }
 fn main() {
-    let target: i32 = 1;
+    let mut target: i32 = 1;
     let nums: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let ret = binary_search(&nums, target);
+    println!("Bs:nums{:?} ,{} position is {}", nums, target, ret);
+    target = 2;
     let ret = binary_search(&nums, target);
     println!("Bs:nums{:?} ,{} position is {}", nums, target, ret);
 }
