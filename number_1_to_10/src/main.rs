4433fbda519dd58ff08b6976ed8d9b11c9c5484c
hironomiu <hironomiu@gmail.com>
2022-08-17 17:00:58 +0900

    commited

--- number_1_to_10/src/main.rs
+++ number_1_to_10/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
-    let num1: Vec<i32> = (1..=10).map(|s| s).collect();
-    println!("{:?}", num1);
+    let nums1: Vec<i32> = (1..=10).map(|s| s).collect();
+    println!("{:?}", nums1);
+
+    let nums2: Vec<i32> = vec![1; 10]
+        .iter()
+        .enumerate()
+        .map(|(i, v)| i as i32 + 1)
+        .collect();
+    println!("{:?}", nums2);
 }
